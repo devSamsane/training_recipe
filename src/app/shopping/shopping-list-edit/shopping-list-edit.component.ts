930d828aea3b1f3c05c253db2758a43b359f8578
Olivier <devangularjs@gmail.com>
2017-11-03 14:10:04 +0100

    feat(shopping-list): add an ingredient

--- src/app/shopping/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../shared/models/ingredient.model';
+import { Component, ElementRef, EventEmitter, OnInit, ViewChild, Output } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('name') nameRef: ElementRef;
+  @ViewChild('count') countRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    const newIngredient = new Ingredient(
+      this.nameRef.nativeElement.value,
+      this.countRef.nativeElement.value
+    );
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
